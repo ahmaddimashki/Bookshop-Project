fbd2d4e5f3e3bdad237134761674ad21cb2850cc
ahmaddimashki <47057524+ahmaddimashki@users.noreply.github.com>
2020-12-15 20:07:19 +0100

    Creating a single page for every book
    * Creating a single page of details for every book and making it responsive.
    * Creating a page for 'read more' button that exists in main page and making it responsive.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Fetching books on main html page //
 let allBooks = document.querySelector(".books");
+
 window.addEventListener("DOMContentLoaded", getBooks);
 function getBooks() {
   fetch("https://bookshop-api.mirkwood.dev/books/")
@@ -7,19 +8,21 @@ function getBooks() {
       return re.json();
     })
     .then(function (data) {
-      let bookDiv = "";
-      data.forEach(function (book) {
-        let title = `<a class = 'title' href= "books/book.html?id=${book.id}">  ${book.title}</a>  `;
-        console.log(title);
-        let author = `<div class = 'author'> ${book.author.first_name} ${book.author.last_name}</div> `;
-        let bookCover = `<img class = 'book-cover' src = ${book.cover_url} >`;
-
-        bookDiv += `<div class='book'> ${title} ${author} ${bookCover}   </div> `;
-      });
-      allBooks.innerHTML = bookDiv;
+      return displayBooks(data);
     })
-
     .catch(function (err) {
       console.log(err);
     });
+}
+
+function displayBooks(data) {
+  let bookDiv = "";
+  data.forEach((book) => {
+    let title = `<a class = 'title' href= "books/book.html?id=${book.id}">  ${book.title}</a>  `;
+
+    let author = `<div class = 'author'> ${book.author.first_name} ${book.author.last_name}</div> `;
+    let bookCover = `<img class = 'book-cover' src = ${book.cover_url} >`;
+    bookDiv += `<div class='book'> ${title} ${author} ${bookCover}   </div> `;
+  });
+  allBooks.innerHTML = bookDiv;
 }
